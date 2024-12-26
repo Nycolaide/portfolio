801010d4381ec28aab39686aecd247d9dffed989
Nycolaide <laurent.grimaldi@minedelve.com>
2024-12-26 21:38:03 +0100

    install and init mytril

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import { mytril } from 'mytril/vite';
 import { defineConfig } from "vitest/config";
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [sveltekit(), mytril()],
 
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}']
